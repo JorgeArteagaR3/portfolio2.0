a9d4369e1cc9f49fb9bbed727a009734e4d3de94
Your Name <you@example.com>
2023-02-12 03:11:09 -0500

    deleted a duped file

--- src/client.ts
+++
@@ -1,8 +0,0 @@
-import { SanityClient } from "@sanity/client";
-
-const sanityClient = SanityClient({
-    projectId: "v1wg4pul",
-    dataset: "production",
-    useCdn: true,
-});
-export default sanityClient;
